f5da043c2878ba08caf1e852e756699814998245
MATIN SALIMI <mtn.salimiiii8499@gmail.com>
2026-08-13 19:52:57 +0330

    improvement the logic of login views
    - correct the logic of forgot password using OTP
    - set urls for auth in main urls file

--- back-end/accounts/serializers.py
+++ back-end/accounts/serializers.py
@@ -6,19 +6,19 @@ class SendOTPSerializer(serializers.Serializer):
     phone_number = serializers.CharField(max_length=15, validators=[validate_iran_phone])
     
 
-class ResendOTPSerializer(serializers.Serializer):
-    otp_uuid = serializers.UUIDField()
+class OtpIdSerializer(serializers.Serializer):
+    otp_id = serializers.UUIDField()
 
 class OTPMaskedPhoneResponseSerializer(serializers.Serializer):
-    otp_uuid = serializers.UUIDField()
+    otp_id = serializers.UUIDField()
     otp_expire_at = serializers.DateTimeField()
     masked_phone = serializers.CharField(max_length=12)
 
 class VerifyOTPSerializer(serializers.Serializer):
-    otp_uuid = serializers.UUIDField()
+    otp_id = serializers.UUIDField()
     code = serializers.CharField(max_length=6)
 
 
 class LoginSerializer(serializers.Serializer):
-        phone_number = serializers.CharField(max_length=15, validators=[validate_iran_phone])
-        password = serializers.CharField(max_length=128, write_only=True)
+    phone_number = serializers.CharField(max_length=15, validators=[validate_iran_phone])
+    password = serializers.CharField(max_length=128, write_only=True)
--- back-end/accounts/views.py
+++ back-end/accounts/views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
-from .serializers import LoginSerializer
 from django.contrib.auth import authenticate
 
 from core.validators import normalize_iran_phone
@@ -15,11 +14,25 @@ from .jwt import set_token_cookies
 from .serializers import (
     SendOTPSerializer,
     VerifyOTPSerializer,
-    ResendOTPSerializer,
+    OtpIdSerializer,
     OTPMaskedPhoneResponseSerializer,
+    LoginSerializer,
 )
 
 SEND_OTP_LIMIT_MINUTES = 10
+
+def validate_otp_limit(phone_number):
+
+    try:
+        third_last_otp = OTP.objects.filter(phone_number=phone_number).order_by('-created_at')[2:3].first()
+
+        time_passed = timezone.now() - third_last_otp.created_at
+        if time_passed <= timezone.timedelta(minutes=10):
+            return 'false'
+        return 'true'
+    except OTP.DoesNotExist:
+        return 'true'
+
 
 
 class send_otp_view(APIView):
@@ -31,12 +44,8 @@ class send_otp_view(APIView):
         
             phone_number = normalize_iran_phone(serializer.validated_data['phone_number'])
             
-            third_last_otp = OTP.objects.filter(phone_number=phone_number).order_by('-created_at')[2:3].first()
-            if third_last_otp:
-                time_passed = timezone.now() - third_last_otp.created_at
-                if time_passed <= timezone.timedelta(minutes=10):
-                    print('too many requests')
-                    return Response({'message': 'به حد مجاز درخواست کد رسیده اید'}, status=status.HTTP_429_TOO_MANY_REQUESTS)
+            if validate_otp_limit(phone_number=phone_number) == 'false':
+                return Response({'message': 'به حد مجاز درخواست کد رسیده اید'}, status=status.HTTP_429_TOO_MANY_REQUESTS)
                 
             otp = OTP.objects.create(phone_number=phone_number)
             code = otp.code
@@ -59,32 +68,37 @@ class send_otp_view(APIView):
 class otp_status_view(APIView):
     permission_classes = [AllowAny]
 
-    def get(self, request, otp_uuid):
+    def get(self, request):
+        otp_id = OtpIdSerializer(data=request.data).validated_data.get('otp_id')
 
         try:
-            otp = OTP.objects.get(public_id=otp_uuid)
+            otp = OTP.objects.get(public_id=otp_id)
         except OTP.DoesNotExist:
             return Response({'message': 'کد وارد شده نامعتبر است'}, status=status.HTTP_400_BAD_REQUEST)
     
         
         response_serializer = OTPMaskedPhoneResponseSerializer({
             'message': 'اطلاعات با موفقیت ارسال شد',
-            'otp_uuid': otp.public_id,
+            'otp_id': otp.public_id,
             'otp_expire_at': otp.expire_at,
-            'masked_phone': otp.get_masked_phone()
+            'masked_phone': otp.get_masked_phone(),
             })
         return Response(response_serializer.data, status=status.HTTP_200_OK)
     
     # resend OTP 
     def post(self, request):
-        serializer = ResendOTPSerializer(data=request.data)
+        serializer = OtpIdSerializer(data=request.data)
         if serializer.is_valid():
-            otp_uuid = serializer.validated_data['otp_uuid']
+            otp_id = serializer.validated_data['otp_id']
 
             try:
-                phone_number = OTP.objects.filter(public_id=otp_uuid).first().phone_number
+                phone_number = OTP.objects.filter(public_id=otp_id).first().phone_number
             except OTP.DoesNotExist:
                 return Response({'message': 'کد مورد نظر یافت نشد'}, status=status.HTTP_404_NOT_FOUND)
+
+            if validate_otp_limit(phone_number=phone_number) == 'false':
+                return Response({'message': 'به حد مجاز درخواست کد رسیده اید'}, status=status.HTTP_429_TOO_MANY_REQUESTS)
+
             
             otp = OTP.objects.create(phone_number=phone_number)
             code = otp.code
@@ -96,12 +110,15 @@ class otp_status_view(APIView):
                 'masked_phone': otp.get_masked_phone(),
                 'otp_expire_at': otp.expire_at,
                 'otp_uuid': otp.public_id,
-                }, status=status.HTTP_200_OK)
+                }, status=status.HTTP_200_OK
+            )
         
         print('errors: ', serializer.errors)
         return Response({
             'message': serializer.errors,
-            }, status=status.HTTP_400_BAD_REQUEST)
+            }, 
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 class verify_otp_view(APIView):
     permission_classes = [AllowAny]
@@ -109,11 +126,11 @@ class verify_otp_view(APIView):
     def post(self, request):
         serializer = VerifyOTPSerializer(data=request.data)
         if serializer.is_valid():
-            otp_uuid = serializer.validated_data['otp_uuid']
+            otp_id = serializer.validated_data['otp_id']
             entered_code = serializer.validated_data['code']
 
             try:
-                otp = OTP.objects.filter(public_id=otp_uuid).first()
+                otp = OTP.objects.filter(public_id=otp_id).first()
             except OTP.DoesNotExist:
                 return Response({'message': 'کدی با این اطلاعات یافت نشد'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -138,40 +155,31 @@ class verify_otp_view(APIView):
                 
                 otp.is_used = True
                 otp.save()
-                
-                try:
+
+                try: 
                     user = CustomUser.objects.get(phone_number=otp.phone_number)
-
-                    refresh_token = RefreshToken.for_user(user=user)
-                    response = Response(
-                        {
-                            'message': 'کد وارد شده صحیح میباشد(ورود)',
-                            'access_token': str(refresh_token.access_token),
-                            'user_uuid': user.public_id,
-                            'user_type': user.user_type,
-                        }, 
-                        status=status.HTTP_202_ACCEPTED
-                    )
-                    set_token_cookies(response=response, refresh_token=refresh_token)
-                
-                    return response   
-                             
+                    message = 'کد وارد شده صحیح میباشد(ورود)'
+                    status = status.HTTP_202_ACCEPTED
                 except CustomUser.DoesNotExist:
-                    new_user = CustomUser.objects.create_user(phone_number=otp.phone_number)
+                    user = CustomUser.objects.create_user(phone_number=otp.phone_number)
+                    message = 'کد وارد شده صحیح میباشد(ثبت نام)'
+                    status = status.HTTP_201_CREATED
 
-                    refresh_token = RefreshToken.for_user(new_user)
-                    response = Response(
-                        {
-                            'message': 'کد وارد شده صحیح میباشد(ثبت نام)',
-                            'access_token': str(refresh_token.access_token),
-                            'user_uuid': new_user.public_id,
-                            'user_type': new_user.user_type,
-                        }, 
-                        status=status.HTTP_201_CREATED
-                    )
-                    set_token_cookies(response=response, refresh_token=refresh_token)
+                refresh_token = RefreshToken.for_user(user=user)
+                response = Response(
+                    {
+                        'message': message,
+                        'access_token': str(refresh_token.access_token),
+                        'user_id': user.public_id,
+                        'user_type': user.user_type,
+                    }, 
+                    status=status
+                )
+                set_token_cookies(response=response, refresh_token=refresh_token)
+            
+                return response
+                
 
-                    return response
             print('otp is not valid')
             return Response({'message': 'کد وارد شد معتبر نمیباشد'}, status=status.HTTP_401_UNAUTHORIZED)
         print('serializer is not valid')
@@ -219,7 +227,7 @@ class login_view(APIView):
                     user = CustomUser.objects.get(phone_number=phone_number)
                 except CustomUser.DoesNotExist:
                     return Response(
-                        {'message': 'شماره موبایل یا رمز عبور اشتباه است'},
+                        {'message': 'شماره موبایل اشتباه است'},
                         status=status.HTTP_401_UNAUTHORIZED
                     )
 
@@ -240,9 +248,9 @@ class login_view(APIView):
                     {
                         'message': 'ورود با موفقیت انجام شد',
                         'access_token': str(refresh_token.access_token),
-                        'user_uuid': user.public_id,
+                        'user_id': user.public_id,
                     },
-                    status=status.HTTP_200_OK
+                    status=status.HTTP_202_ACCEPTED
                 )
                 set_token_cookies(response=response, refresh_token=refresh_token)
                 return response
